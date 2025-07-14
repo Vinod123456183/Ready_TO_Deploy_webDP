82f0b29a8c382b4c52b23a12440fdc3b3b605577
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-07-14 10:47:03 -0700

    Add files via upload

---
+++ ___Important___REACT__Tailwind___Complete__/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-3xs font-semibold">Hello</div>
+    </>
+  );
+}
+
+export default App;
